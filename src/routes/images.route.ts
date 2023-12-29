444f823f8de34870ac5c4a5d863372e3c8cf0e0a
MeesterMarcus <lorenzanamarcus@gmail.com>
2023-12-29 16:43:32 -0600

    successfully retrieving objects from passed in image url and saving to db

--- src/routes/images.route.ts
+++ src/routes/images.route.ts
@@ -1,25 +1,19 @@
 import { Router, Request, Response } from "express";
 import { validateImageGetParams } from "../middleware/images.middleware";
-import { basicAuth } from "../middleware/auth.middleware"; // Import the middleware
-import axios from "axios";
 import { Image } from "../schemas/image";
 import ImageService from "../services/Images.service";
 
 const router = Router();
 const baseUrl = '/images'
 
-router.get(`${baseUrl}/test`, basicAuth, async (req: Request, res: Response): Promise<Response> => {
+router.get(`${baseUrl}/test`, async (req: Request, res: Response): Promise<Response> => {
     const imageUrl = 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/90/Labrador_Retriever_portrait.jpg/1200px-Labrador_Retriever_portrait.jpg';
-    const url = `https://api.imagga.com/v2/tags?image_url=${encodeURIComponent(imageUrl)}`;
-
+    if (!req.headers.authorization) {
+        throw new Error("Missing authorization")
+    }
     try {
-        const response = await axios.get(url, {
-            headers: {
-                'Authorization': req.headers.authorization // Use the authorization header set in the middleware
-            }
-        });
-
-        return res.json(response.data);
+        const response = await ImageService.parseImage(imageUrl, req.headers.authorization);
+        return res.json(response);
     } catch (error) {
         console.error('Error making the API request', error);
         return res.status(500).send('Error making the API request');
@@ -35,8 +29,11 @@ router.get(`${baseUrl}`, validateImageGetParams, async (req: Request, res: Respo
 });
 
 router.post(`${baseUrl}`, async (req: Request, res: Response): Promise<Response> => {
+    if (!req.headers.authorization) {
+        throw new Error("Missing authorization")
+    }
     try {
-        const result = await ImageService.createImage(req.body);
+        const result = await ImageService.createImage(req.body, req.headers.authorization);
         return res.status(200).send({
             message: "Successfully saved image document",
             body: result
--- src/services/Images.service.ts
+++ src/services/Images.service.ts
@@ -1,16 +1,16 @@
+import axios from "axios";
 import { Image } from "../schemas/image";
 import { faker } from '@faker-js/faker';
 
 class ImageService {
-    static async createImage(body: any) {
+    static async createImage(body: any, authorizationHeader: string) {
         const imgData = body.imgData;
         const label = body.label ? body.label : faker.datatype.uuid();
         const enableObjectDetection = body.enableObjectDetection;
-        const objects: string[] = [];
+        let objects: string[] = [];
 
-        // TODO: use imagga or other object detection logic here
         if (enableObjectDetection) {
-            objects.push('dog'); // This should be replaced with real object detection results
+            objects = await this.parseImage(imgData, authorizationHeader)
         }
 
         const entity = { imgData, label, objects };
@@ -22,6 +22,31 @@ class ImageService {
             _id: result._id,
             ...entity
         };
+    }
+
+    static async parseImage(imageUrl: string, authorizationHeader: string) {
+        const url = `https://api.imagga.com/v2/tags?image_url=${encodeURIComponent(imageUrl)}`;
+        try {
+            const response = await axios.get(url, {
+                headers: {
+                    'Authorization': authorizationHeader
+                }
+            });
+            return ImageService.extractHighConfidenceTags(response.data);
+        } catch (error) {
+            // Rethrow the error or create a custom error as needed
+            throw error;
+        }
+    }
+
+    private static extractHighConfidenceTags(response: any): string[] {
+        // Accessing the tags array
+        const tags = response.result.tags;
+    
+        // Filtering and mapping
+        return tags
+            .filter((tag: any) => tag.confidence > 70) // Filter tags with confidence > 70
+            .map((tag: any) => tag.tag.en); // Map over the filtered tags to extract the 'en' value
     }
 }
 
